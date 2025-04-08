93a6e5de7f72d32b17ff0d1d001890fa9aa5d8b4
byubraden <byubraden26@gmail.com>
2025-04-08 10:28:15 -0600

    updates

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,23 +1,74 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import Logout from '../components/Logout';
 import Slideshow from '../components/Slideshow';
 import Recommender from '../components/RecommenderComponent';
 
 const LandingPage: React.FC = () => {
+  useEffect(() => {
+    if (document.querySelector('script[src="/slideshow.js"]')) return;
+
+    const script = document.createElement('script');
+    script.src = '/slideshow.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      if (window.initSlideshow) {
+        window.initSlideshow();
+      }
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <div className="home-container">
-      {/* <span>
-          <Logout>
-            Logout <AuthorizedUser value="email" />
-          </Logout>
-        </span> */}
-      <Slideshow />
-      <div className="content">
-        <Recommender Id={1} Name="Whats Popular" />
-        <Recommender Id={2} Name="Editor Picks" />
-      </div>
-    </div>
+    <>
+      {/* <Slideshow /> */}
+    </>
   );
 };
 
 export default LandingPage;
+
+// import React, { useEffect } from 'react';
+// // import Logout from '../components/Logout';
+// import Slideshow from '../components/Slideshow';
+// import Recommender from '../components/RecommenderComponent';
+
+// useEffect(() => {
+//   const script = document.createElement('script');
+//   script.src = '/slideshow.js';
+//   script.async = true;
+//   document.body.appendChild(script);
+
+//   script.onload = () => {
+//     if (window.initSlideshow) {
+//       window.initSlideshow();
+//     }
+//   };
+
+//   return () => {
+//     document.body.removeChild(script);
+//   };
+// }, []); // 👈 this empty array is critical!
+
+// const LandingPage: React.FC = () => {
+//   return (
+//     // <div className="home-container">
+//     //   {/* <span>
+//     //       <Logout>
+//     //         Logout <AuthorizedUser value="email" />
+//     //       </Logout>
+//     //     </span> */}
+//       <Slideshow />
+//     //   {/* <div className="content">
+//     //     <Recommender Id={1} Name="Whats Popular" />
+//     //     <Recommender Id={2} Name="Editor Picks" />
+//     //   </div>
+//     // </div> */}
+//   );
+// };
+
+// export default LandingPage;
