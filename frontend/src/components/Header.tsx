a9b3869e652b936d3c13bc5f5983698133f8f473
Jaden Taylor <jtjadent@gmail.com>
2025-04-08 16:52:02 -0600

    account dummy dropdown works

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './Header.css';
 import { FaSearch, FaUserCircle } from 'react-icons/fa';
 import logo from '../images/cinenicheicon_720.png';
@@ -6,13 +6,19 @@ import { UserContext } from './AuthorizeView';
 import { useNavigate } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  //authentication stuff
   const user = useContext(UserContext);
   const isLoggedIn = !!user;
 
+  //navigation stuff
   const navigate = useNavigate();
   const handleNavigation = (path: string) => {
     navigate(path);
   };
+
+  //profile dropdown stuff
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
 
   return (
     <>
@@ -27,7 +33,32 @@ const Header: React.FC = () => {
             <button className="btn" onClick={() => handleNavigation('/MyListPage')}>My List</button>
             <button className="btn" onClick={() => handleNavigation('/AdminPage')}>Admin</button>
             <FaSearch className="icon" />
-            <FaUserCircle className="icon profile-icon" />
+            {/* Profile Icon and Dropdown */}
+
+          <div className="icon profile-icon" onClick={() => setDropdownOpen(!isDropdownOpen)}>
+            <FaUserCircle/>
+          </div>
+
+          <div className={`profile-dropdown ${isDropdownOpen ? "active" : ""}`}>
+            <h4>Profile</h4>
+            <label>
+              Make Phone Number Public
+              <input type="checkbox" />
+            </label>
+            <label>
+              Make Personality Profile Public
+              <input type="checkbox" />
+            </label>
+            <button>Logout</button>
+            {/* <button onClick={handleLogout} className="logout-button">
+              Logout
+            </button> */}
+          </div>
+            {/* Todo: make the FaUserCircle create a dropdown menu with the following items:
+            - Account
+            - Privacy Policy
+            - Logout */}
+            {/* <FaUserCircle className="icon profile-icon" /> */}
           </div>
         </header>
       ) : (
