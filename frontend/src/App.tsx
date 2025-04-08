ad2b29eb5cc88176946c674593f65b930709a448
Jaden Taylor <jtjadent@gmail.com>
2025-04-08 11:18:23 -0600

    routing works! Logged in at least

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,6 @@ function App() {
           path="/"
           element={
             <AuthorizeView>
-              <Header/>
               <HomePage/>
             </AuthorizeView>
           }
