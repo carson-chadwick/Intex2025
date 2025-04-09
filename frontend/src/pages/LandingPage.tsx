ea121375dbb084b270dc635ef9880e5ab90ca410
byubraden <byubraden26@gmail.com>
2025-04-09 14:12:58 -0600

    simplify recommender component

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -3,27 +3,35 @@ import { useNavigate } from 'react-router-dom';
 import rick from '../images/rick.png';
 import PrivacyPage from './PrivacyPage';
 import Footer from '../components/Footer';
+import Recommender from '../components/RecommenderComponent';
 function LandingPage() {
+  const navigate = useNavigate();
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+  return (
+    <>
+      <Header />
+      <h1>You got rick rolled</h1>
+      <img src={rick} />
+      <p>
+        This is the landing page. This is the first page that logged out users
+        see.
+      </p>
+      <button className="btn" onClick={() => handleNavigation('/login')}>
+        Login
+      </button>
+      <button className="btn" onClick={() => handleNavigation('/register')}>
+        Register
+      </button>
 
-    const navigate = useNavigate();
-    const handleNavigation = (path: string) => {
-      navigate(path);
-    };
-    return (
-        <>  
-            <Header/>
-            <h1>You got rick rolled</h1>
-            <img src={rick}/>
-            <p>
-                This is the landing page. 
-                This is the first page that logged out users see.
-            </p>
-            <button className="btn" onClick={() => handleNavigation('/login')}>Login</button>
-            <button className="btn" onClick={() => handleNavigation('/register')}>Register</button>
-            <Footer/>
-        </>
-    );
+      <Recommender Name="Top Hits" type="topHits" />
+      <Recommender Name="Editors Picks" type="editorsPicks" />
+      <Recommender Name="Recently Added" type="recentlyAdded" />
+
+      <Footer />
+    </>
+  );
 }
-
 
 export default LandingPage;
