f37ce27790313fa3a83adca6984f8b9aa935af46
Jaden Taylor <jtjadent@gmail.com>
2025-04-10 14:27:59 -0600

    small changes

--- frontend/src/pages/AccountPage 2.tsx
+++ frontend/src/pages/AccountPage 2.tsx
@@ -1,20 +1,22 @@
-import AuthorizeView from "../components/AuthorizeView";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+//We may not need this file. If the website continues to work without it, then delete.
 
-function AccountPage() {
+// import AuthorizeView from "../components/AuthorizeView";
+// import Footer from "../components/Footer";
+// import Header from "../components/Header";
 
-    return(
-        <>
-            <AuthorizeView>
-                <Header/>
-                <h1>Congrats, you made it to the account page.</h1>
-                <h2>Change your password here. </h2>
-                <h2>Update your profile Pic here.</h2>
-                <Footer/>
-            </AuthorizeView>
-        </>
-    );
-}
+// function AccountPage() {
 
-export default AccountPage;
+//     return(
+//         <>
+//             <AuthorizeView>
+//                 <Header/>
+//                 <h1>Congrats, you made it to the account page.</h1>
+//                 <h2>Change your password here. </h2>
+//                 <h2>Update your profile Pic here.</h2>
+//                 <Footer/>
+//             </AuthorizeView>
+//         </>
+//     );
+// }
+
+// export default AccountPage;
